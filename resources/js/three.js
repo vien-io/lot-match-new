767f92710b8117d6cf125a322d9c3f4427a7bbbe
vien <comania22-0532@cca.edu.ph>
2025-10-16 03:19:30 +0800

    feat(threejs): implement working OutlinePass highlight for lots; plan block-level merge optimization
    - Added OutlinePass highlighting for individual lots within InstancedMesh
    - Fixed metadata type mismatch (blockId) causing missing merges
    - Outline highlighting now triggers correctly when hovering markers
    - Next goal: optimize performance by merging lots per block into a single geometry
      for block-level highlighting (reduces draw calls)

--- resources/js/three.js
+++ resources/js/three.js
@@ -1,7 +1,14 @@
+import * as THREE from 'three';
 import { Chart, registerables } from 'chart.js';
 Chart.register(...registerables);
 import 'chartjs-adapter-date-fns';
 import Stats from 'three/examples/jsm/libs/stats.module.js';
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
+import { OutlinePass } from 'three/examples/jsm/postprocessing/OutlinePass.js';
+import { FXAAShader } from 'three/examples/jsm/shaders/FXAAShader.js';
+import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js';
+
 
 // gsap for cam animation
 import gsap from "gsap";
@@ -13,6 +20,7 @@ import { initLights } from './threePartials/initLights';
 import { initControls } from './threePartials/initControls';
 import { initHelpers } from './threePartials/initHelpers';
 import { loadHouses } from './threePartials/loadHouses';
+import { initRaycasterOutlinePass } from './threePartials/initRaycasterOutlinePass';
 import { initRaycaster } from './threePartials/initRaycaster';
 import { initClickHandler } from './threePartials/initClickHandler';
 import { fetchForecast } from './threePartials/forecastHandler';
@@ -32,14 +40,52 @@ function initThreeJS() {
 
     const { housesGroup, selectableObjects, instanceMetadata } = loadHouses(scene);
 
-    initRaycaster({ 
+    // outline pass setup
+    const composer = new EffectComposer(renderer);
+    const renderPass = new RenderPass(scene, camera);
+    composer.addPass(renderPass);
+
+    const outlinePass = new OutlinePass(
+        new THREE.Vector2(container.clientWidth, container.clientHeight),
+        scene,
+        camera
+    );
+
+    outlinePass.edgeStrength = 5;      
+    outlinePass.edgeGlow = 0.5;
+    outlinePass.edgeThickness = 2;
+    outlinePass.visibleEdgeColor.set('#ffff00');
+    outlinePass.hiddenEdgeColor.set('#ffff00');  
+    composer.addPass(outlinePass);
+
+
+    const fxaaPass = new ShaderPass(FXAAShader);
+    fxaaPass.material.uniforms['resolution'].value.set(
+        1 / container.clientWidth,
+        1 / container.clientHeight
+    );
+    composer.addPass(fxaaPass);
+
+    initRaycasterOutlinePass({
+        container,
+        scene,
+        camera,
+        renderer,
+        housesGroup,
+        selectableObjects,
+        instanceMetadata,
+        outlinePass
+    });
+
+   /*  initRaycaster({ 
         container, 
         camera, 
         renderer, 
         housesGroup, 
         selectableObjects, 
         instanceMetadata
-    });
+    }); */
+    
     initClickHandler({
         camera,
         renderer,
@@ -60,8 +106,8 @@ function initThreeJS() {
     function animate() {
         requestAnimationFrame(animate);
         controls.update();
-        renderer.render(scene, camera);
-        stats.update();
+        composer.render();
+        // stats.update();
     }
     animate();
 }
